57d714ae406daca4da5d3e42a9e0ccc058ab9035
Feiou Zhang <feiou.zhang@hotmail.com>
2017-08-26 14:07:08 -0500

    bubblesort

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] nums = new int[]{8,0};
+        BubbleSort bs = new BubbleSort();
+        bs.bubbleSort(nums);
+        System.out.print(Arrays.toString(nums));
     }
 }
